dfa779e55f1f1a57c0faec30cba8b5601c34c060
Shreenath Bharadwaj <bharadwahshreenath@gmail.com>
2020-10-01 21:14:20 +0530

    make it better

--- plant_classifier_project/settings.py
+++ plant_classifier_project/settings.py
@@ -124,8 +124,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
